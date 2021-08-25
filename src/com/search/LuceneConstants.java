8b77c5085f103d8d51a21de7092afe455491870a
Mani-mka <manishsachin.mk7514@gmail.com>
2021-08-25 23:00:51 +0530

    Creating documents from files and searching

--- src/com/search/LuceneConstants.java
+++ src/com/search/LuceneConstants.java
@@ -23,10 +23,11 @@ import java.nio.file.Path;
 public class LuceneConstants {
     public static void main(String[] args) {
         try {
+            // Path where you want indices to be stored
             Path indexPath = FileSystems.getDefault().getPath("D:\\Projects\\Java Projects\\index for documents");
             Directory dir = FSDirectory.open(indexPath);
-            File file = new File("D:\\Study Materials\\IntelliJ shortcuts.txt");
-            writeDocFromFile(dir, file);
+            //File file = new File("D:\\Study Materials\\IntelliJ shortcuts.txt");
+            //writeDocFromFile(dir, file);
             //writeDoc(dir);
             searchDoc(dir, "content", "commit");
         } catch (IOException ex) {
@@ -36,17 +37,21 @@ public class LuceneConstants {
 
     public static void searchDoc(Directory dir, String field, String text){
         try {
+            // Read from the directory that has indices and run query on it
             DirectoryReader reader = DirectoryReader.open(dir);
             IndexSearcher searcher = new IndexSearcher(reader);
             TermQuery tq = new TermQuery(new Term(field, text));
             TopDocs results = searcher.search(tq, 20);
             System.out.println("Match found in " + results.scoreDocs.length + " documents");
+
+            //print details of the matched documents
             for(ScoreDoc scoreDoc: results.scoreDocs){
                 // System.out.println("document ID: " + scoreDoc.doc + "\n" + "Score: " + scoreDoc.score );
                 Document doc = reader.document(scoreDoc.doc);
                 String content = doc.get("content");
                 System.out.println("Content of the document is: " + content);
             }
+            // close the reader
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -55,13 +60,18 @@ public class LuceneConstants {
 
     public static void writeDoc(Directory dir){
         try {
+            // perform analysis using standard analyzer
             Analyzer analyzer = new StandardAnalyzer();
             IndexWriterConfig icw = new IndexWriterConfig(analyzer);
             IndexWriter writer = new IndexWriter(dir, icw);
+
+            // create a new document and index it
             Document doc = new Document();
             doc.add(new TextField("content", "I was there", Field.Store.YES));
             doc.add(new StringField("students", "trust fund baby", Field.Store.YES));
             writer.addDocument(doc);
+
+            // closes and commits the indexing operation
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
